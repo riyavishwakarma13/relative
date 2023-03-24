68acd16c567a9c800ace5cd8bafba3701199c8c2
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2023-03-24 22:59:20 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,81 @@
-import './App.css';
-import Card from './components/Card';
+import "./App.css";
+import Card from "./components/Card";
+
+const cardValues = [
+  {
+    headerImage: "/Bitcoin SV (BSV).png",
+    title: "Bitcoin (BTC)",
+    coinPrice: "$31,812.80",
+    coinPercentage: +10,
+    price: "$60,000",
+    popularPairs: [
+      "/Solana (SOL).png",
+      "/Ethereum (ETH).png",
+      "/Binance Coin (BNB).png",
+    ],
+  },
+  {
+    headerImage: "/Solana (SOL).png",
+    title: "Solana (SOL)",
+    coinPrice: "$32.83",
+    coinPercentage: -12.32,
+    price: "$60,000",
+    popularPairs: [
+      "/Bitcoin SV (BSV).png",
+      "/Ethereum (ETH).png",
+      "/Binance Coin (BNB).png",
+    ],
+  },
+  {
+    headerImage: "/Ethereum (ETH).png",
+    title: "Ethereum (ETH)",
+    coinPrice: "$1,466.45",
+    coinPercentage: -11.93,
+    price: "$60,000",
+    popularPairs: [
+      "/Solana (SOL).png",
+      "/Bitcoin SV (BSV).png",
+      "/Binance Coin (BNB).png",
+    ],
+  },
+  {
+    headerImage: "/Binance Coin (BNB).png",
+    title: "Binance USD (BUSD)",
+    coinPrice: "$1.00",
+    coinPercentage: 0.26,
+    price: "$60,000",
+    popularPairs: [
+      "/Solana (SOL).png",
+      "/Ethereum (ETH).png",
+      "/Binance Coin (BNB).png",
+    ],
+  },
+  {
+    headerImage: "/SHIBA INU (SHIB).png",
+    title: "Shiba Inu (SHIB)",
+    coinPrice: "$0.00000001948",
+    coinPercentage: -8.1,
+    price: "$60,000",
+    popularPairs: [
+      "/Solana (SOL).png",
+      "/Ethereum (ETH).png",
+      "/Binance Coin (BNB).png",
+    ],
+  },
+];
 
 function App() {
   return (
-    <div className="bg-primary min-h-[100vh] flex flex-col py-10 px-5">
+    <div className="bg-primary min-h-[100vh] flex flex-col py-20 px-5">
       <div className="flex items-center gap-5">
         <img src="/activity.png" alt="tailwindcss logo" className="w-5 h-5" />
         <h1 className="text-xl text-white">Trending Assets</h1>
       </div>
-      <Card />
+      <div className="flex flex-wrap gap-10 justify-center items-center my-20 ">
+        {cardValues.map((cardValue, index) => (
+          <Card key={index} {...cardValue} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,40 +1,48 @@
-const Card = () => {
+interface Props {
+  headerImage: string;
+  title: string;
+  coinPrice: string;
+  coinPercentage: number;
+  price: string;
+  popularPairs: string[];
+}
+
+const Card = ({
+  coinPercentage,
+  coinPrice,
+  headerImage,
+  popularPairs,
+  price,
+  title,
+}: Props) => {
   return (
     <div className="flex flex-col justify-center items-center relative ">
       <div className="w-[100px] h-[100px] card-bg rounded-full flex items-center justify-center relative">
-        <img
-          src="/Bitcoin SV (BSV).png"
-          alt="tailwindcss logo"
-          className="w-12 h-12 absolute"
-        />
+        <img src={headerImage} alt={title} className="w-12 h-12 absolute" />
       </div>
       <div className="card-image ">
-        <h1 className="text-secondary font-semibold">Bitcoin (BTC)</h1>
+        <h1 className="text-secondary font-semibold">{title}</h1>
         <div className="w-[80%] flex gap-4 items-center justify-end bg-primary rounded-full px-4 py-1">
-          <p className="text-white  font-semibold">$31,812.80</p>
-          <p className={`text-green-500`}>+10%</p>
+          <p className="text-white  font-semibold">{coinPrice}</p>
+          <p
+            className={
+              coinPercentage >= 0
+                ? `text-green-500 font-semibold`
+                : `text-red-500 font-semibold`
+            }
+          >
+            {coinPercentage}%
+          </p>
         </div>
         <p className="text-secondary text-sm">Price</p>
         <div className="w-[80%] bg-primary rounded-full px-4 py-1">
-          <p className="text-white text-center  font-semibold">$60,000</p>
+          <p className="text-white text-center  font-semibold">{price}</p>
         </div>
         <p className="text-secondary font-semibold text-sm">TVL</p>
         <div className=" bg-primary rounded-full px-3 py-2 flex gap-3">
-          <img
-            src="/Bitcoin SV (BSV).png"
-            alt="tailwindcss logo"
-            className="w-6 h-6 "
-          />
-          <img
-            src="/Bitcoin SV (BSV).png"
-            alt="tailwindcss logo"
-            className="w-6 h-6 "
-          />
-          <img
-            src="/Bitcoin SV (BSV).png"
-            alt="tailwindcss logo"
-            className="w-6 h-6 "
-          />
+          {popularPairs.map((pair, index) => (
+            <img key={index} src={pair} alt={title} className="w-6 h-6 " />
+          ))}
         </div>
         <p className="text-sm text-secondary font-semibold">Popular pairs</p>
       </div>
