8b962002af91250fbb4ea7c7a3d3f5b45877a45b
Riya Vishwakarma <riyavishwakarma.alf@gmail.com>
2023-03-24 21:31:48 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary min-h-[100vh] flex flex-col py-10 px-5">
+      <div className="flex items-center gap-5">
+        <img src="/activity.png" alt="tailwindcss logo" className="w-5 h-5" />
+        <h1 className="text-xl text-white">Trending Assets</h1>
+      </div>
+      <div className="flex flex-wrap justify-center items-center">
+        <div className="w-[100px] h-[100px] card-bg rounded-full flex items-center justify-center">
+        <img src="/Bitcoin SV (BSV).png" alt="tailwindcss logo" className="w-12 h-12" />
+        </div>
+      </div>
     </div>
   );
 }
